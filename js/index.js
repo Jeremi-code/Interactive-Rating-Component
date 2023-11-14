1355d3bf9acafbe0dcdf2e98224c62aba833c0d0
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-11-14 13:24:40 -0800

    get the clicked rate and change the background color of the clicked span

--- js/index.js
+++ js/index.js
@@ -1,16 +1,34 @@
-const spanElements = document.getElementsByClassName('rating-point')
-console.log(spanElements)
-for (let i=0; i<spanElements.length; i++) {
-    console.log(spanElements[i])
-    spanElements[i].addEventListener('click',()=>{onClick(i)
-    })
+const spanElements = document.getElementsByClassName("rating-point");
+let ClickedRate = null;
+let ClickedElems = null;
+for (let i = 0; i < spanElements.length; i++) {
+  console.log(spanElements[i]);
+  spanElements[i].addEventListener("click", () => {
+    handleClick(i, spanElements[i]);
+  });
 }
-const onClick = (i) => {
-    const rateContainer = document.getElementsByClassName('rate-container')
-    const resultContainer = document.getElementsByClassName('result-container')
-    rateContainer[0].style.display = "none"
-    resultContainer[0].style.display = "flex"
-    const element = document.getElementsByClassName('result-text')
-    console.log(element)
-    element[0].innerHTML = `You selected ${i} out of 5`
-}
+const handleClick = (i, spanElement) => {
+  if (ClickedElems == null) {
+    ClickedElems = spanElement;
+  }
+  ClickedElems.style.backgroundColor = "hsl(213, 19%, 18%)";
+  spanElement.style.backgroundColor = "hsl(217, 12%, 63%)";
+  spanElement.style.color = "white";
+  ClickedRate = i + 1;
+  ClickedElems = spanElement;
+};
+document.querySelector("button").addEventListener("click", () => {
+  handleSubmit(ClickedRate);
+});
+
+const handleSubmit = (ClickedRate) => {
+  if (ClickedRate == null) {
+    return;
+  }
+  const rateContainer = document.getElementsByClassName("rate-container");
+  const resultContainer = document.getElementsByClassName("result-container");
+  rateContainer[0].style.display = "none";
+  resultContainer[0].style.display = "flex";
+  const element = document.getElementsByClassName("result-text");
+  element[0].innerHTML = `You selected ${ClickedRate} out of 5`;
+};
